f493704b1a6d04474fb6171cf2d88b7ba04c6d04
asearle13 <amyjadesearle@gmail.com>
2016-02-16 15:24:38 +0200

    edited

---
+++ src/main/java/com/bbd/nkandla/SwimmingPool.java
@@ -0,0 +1,22 @@
+package com.bbd.nkandla;
+
+public class SwimmingPool {
+	
+	private int length;
+	private int width;
+	
+	public SwimmingPool(int width, int length){
+		this.length = length;
+		this.width = width;
+	}
+
+	public int getLength (){
+		return this.length;
+	}
+	
+	public int getWidth (){
+		return this.width;
+	}
+	
+	
+}
